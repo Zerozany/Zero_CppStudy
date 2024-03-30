d8d0b6a8d3e59c7844b951899fa891a4cf7e28c1
ZongYao Zhu <691139128@qq.com>
2024-03-30 23:27:58 +0800

    目录规范调整(一)

---
+++ C++98-Base/extern_C/extern.cpp
@@ -0,0 +1,7 @@
+#include "myadd.h"
+
+int main(int argc, char const *argv[])
+{
+    add(10,19);
+    return 0;
+}
---
+++ C++98-Base/extern_C/myadd.h
@@ -0,0 +1,15 @@
+
+extern "C" void add(int x, int y);
+
+#if __cplusplus
+extern "C"
+{
+#endif
+
+    void sub(int x, int y);
+
+#if __cplusplus
+}
+#endif
+
+
